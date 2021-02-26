b971456b70566578cf60442fd6ea0cbda334779b
oldUath <haiyangdong666@gmail.com>
2021-02-26 14:20:34 +0800

    deploy

---
+++ precache-manifest.d96d79b2be65f4bd3d4c6eaba02788e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da724c90c8c61385ab36",
+    "url": "/easyMoney-website/css/app.8af64df6.css"
+  },
+  {
+    "revision": "ae3007baaa47b311f2ea01c3aa28cbb9",
+    "url": "/easyMoney-website/index.html"
+  },
+  {
+    "revision": "da724c90c8c61385ab36",
+    "url": "/easyMoney-website/js/app.be105a49.js"
+  },
+  {
+    "revision": "14714058a4a8b10c682b",
+    "url": "/easyMoney-website/js/chunk-vendors.a8891e7e.js"
+  },
+  {
+    "revision": "ea6538a64f5ace04b8ff04a6952b7083",
+    "url": "/easyMoney-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/easyMoney-website/robots.txt"
+  }
+]);
